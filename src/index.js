81a5bf6801d2c70d10714b6ffa2fcf4bec687fff
AndersonRichard <Richard-r3@hotmail.com>
2025-03-26 20:59:09 -0300

    Adicionado arquivos base para estruturação do projeto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
   res.send('Servidor Express está funcionando! 🚀');
 });
 
 
-app.listen(3000, () => {
-  console.log(`Servidor rodando em http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
