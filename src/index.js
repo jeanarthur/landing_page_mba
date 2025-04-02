653b573240308e333f2df9d341aa9cdb9b8563ec
AndersonRichard <Richard-r3@hotmail.com>
2025-04-01 22:10:53 -0300

    Criação do banco de dados

--- src/index.js
+++ src/index.js
@@ -3,22 +3,33 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const connectDB = require("./database/mongo")
 
-
-const express = require('express');
+const lead = require('./models/lead');
 const app = express();
-
-
+app.use(express.json())
 const PORT = process.env.PORT || 3000; 
 
 app.use(express.static(path.join(__dirname, 'app', 'public')));
 
+connectDB()
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
 
+app.post("/teste", async (req, res) => {
+  const dados = req.body
+  console.log(dados)
+  try{
+      const novoLead = new lead(dados)
+      const leadSalvo = await novoLead.save()
 
+      res.status(201).send(leadSalvo)
+  }catch(err){
+      res.status(500).send({ mensagem: "Erro ao criar lead"})
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
